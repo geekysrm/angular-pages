b33c9352b2b473ba97ed53387b8d43b8e0512895
geekysrm <soumyacool2012@gmail.com>
2020-08-30 18:23:28 +0530

    replace all ngClass with custom appCustomNgClass

--- src/app/custom-ng-class.directive.ts
+++ src/app/custom-ng-class.directive.ts
@@ -11,7 +11,19 @@ export class CustomNgClassDirective {
   // @Input() set appCustomNgClass(color: string) {
   //   this.element.nativeElement.style.backgroundColor = color;
   // }
-  @Input('appCustomNgClass') set backgroundColor(color: string) {
-    this.element.nativeElement.style.backgroundColor = color;
+  // Final:
+  // @Input('appCustomNgClass') set backgroundColor(color: string) {
+  //   this.element.nativeElement.style.backgroundColor = color;
+  // }
+  // For different functionality:
+  @Input('appCustomNgClass') set classNames(classObj: any) {
+    // For-in loops can be used to iterate over keys of an object
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
